518d59f13c2b75703e75256db63005f851cf8f27
Robert Tam <rsh.tam@gmail.com>
2019-07-22 15:52:43 +0100

    Add tests for water bottle

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,5 @@
 public class Calculator {
 
-  public Calculator() {
-  }
-
   public double add(double num1, double num2) {
     return num1 + num2;
   }
---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,37 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+  private WaterBottle waterBottle;
+
+  @Before
+  public void before() {
+    waterBottle = new WaterBottle();
+  }
+
+  @Test
+  public void hasVolume() {
+    assertEquals(100, waterBottle.getVolume());
+  }
+
+  @Test
+  public void drinkReducesVolume() {
+    waterBottle.drink();
+    assertEquals(90, waterBottle.getVolume());
+  }
+
+  @Test
+  public void emptyWaterBottle() {
+    waterBottle.empty();
+    assertEquals(0, waterBottle.getVolume());
+  }
+
+  @Test
+  public void fillWaterBottle() {
+    waterBottle.empty();
+    waterBottle.fill();
+    assertEquals(100, waterBottle.getVolume());
+  }
+}
